c6e384068e75f441d32cbef271b938ceadc671ed
ramiro.salom <ramiro.salom@616bce18-80df-bbd3-df9c-2f520cdca58d>
2012-11-14 00:23:28 +0000

    Agregado de BuscadorClases con funciones estaticas Un converter o adapter para adaptar ciertos elementos EslabonComun (todos los eslabones van a ser de esta clase y lo unico que cambia es la regla que contiene) Se configuraron direcctorios SON HARDCODEADOS para que les corra tienen que CAMBIARLOS

--- trunk/Tecnicas/src/ar/com/fi/uba/tecnicas/controlador/ComandosUsuarioFacade.java
+++ trunk/Tecnicas/src/ar/com/fi/uba/tecnicas/controlador/ComandosUsuarioFacade.java
@@ -4,8 +4,9 @@ package ar.com.fi.uba.tecnicas.controlador;
 import java.util.ArrayList;
 import java.util.List;
 
-import ar.com.fi.uba.tecnicas.controlador.validador.ValidadorPadronDeInscripto;
-import ar.com.fi.uba.tecnicas.controlador.validador.ValidadorPadronNuevo;
+import ar.com.fi.uba.tecnicas.Configuracion;
+import ar.com.fi.uba.tecnicas.controlador.comun.Constantes;
+import ar.com.fi.uba.tecnicas.controlador.comun.Converter;
 import ar.com.fi.uba.tecnicas.controlador.validador.ValidadorParametro;
 import ar.com.fi.uba.tecnicas.modelo.entidades.Parametro;
 import ar.com.fi.uba.tecnicas.modelo.entidades.Regla;
@@ -23,28 +24,56 @@ public class ComandosUsuarioFacade {
 	
 	private Repositorio<Regla> repositorioRegla;
 	private List<ValidadorParametro> validadores;
+	private List<String> acciones;
 
 	/**
-	 * 
+	 * Crea la regla interactuando con el usuario
 	 * @param invocador
 	 * @return
 	 */
 	public String crearRegla(InterfazUsuario invocador){
-		if (repositorioRegla == null) {
-			repositorioRegla = new RepositorioReglas();
-		}
-		if (validadores == null) {
-			//reemplazar este new por la llamada a una factory o builder 
-			validadores = new ArrayList<ValidadorParametro>();
-			validadores.add(new ValidadorPadronNuevo());
-			validadores.add(new ValidadorPadronDeInscripto());
-		}
+		inicializar(invocador);
+		
 		Regla regla = new Regla();
 		regla.setNombre(invocador.obtenerDatos("Ingrese el nombre de la regla: "));
 		regla.setAsunto(invocador.obtenerDatos("Ingrese el asunto que será validado: "));
+		
+		agregarParametros(invocador, regla);
+		
+		agregarAcciones(invocador, regla);
+		
+		try {
+			repositorioRegla.agregar(regla);
+		} catch (ValidacionExcepcion e) {
+			invocador.mensaje("No pudo guardarse la nueva regla, puede que ya exista. Por favor intentelo de nuevo.");
+			return "";
+		}
+		return "Se creo la regla.";
+	}
+
+	private void agregarAcciones(InterfazUsuario invocador, Regla regla) {
+		Integer indice;
+		String agregarMasAcciones;
+		
+		do {
+			//Necesito listar los validadores que tengo para los parametros
+			invocador.mensaje("Acciones disponibles para ejecutarse: ");
+			for (String accion : acciones) {
+				invocador.mensaje(acciones.indexOf(accion) + ") " + accion);
+			}
+			indice = Integer.valueOf(invocador.obtenerDatos("Elija la accion para ejecutar: "));
+			if (!regla.addAccion(acciones.get(indice))) {
+				invocador.mensaje("No pudo agregarse el nuevo parametro por favor intentelo de nuevo.");
+			}
+			agregarMasAcciones = invocador.obtenerDatos("Desea especificar más acciones? (Si/No): ");	
+		} while (agregarMasAcciones.equalsIgnoreCase("si"));
+	}
+
+	private void agregarParametros(InterfazUsuario invocador, Regla regla) {
 		Parametro parametro;
-		Integer indice = 0;
-		if (invocador.obtenerDatos("Desea especificar parametros? (Si/No)").equalsIgnoreCase("si")) {
+		Integer indice;
+		String agregarParametros = invocador.obtenerDatos("Desea especificar parametros? (Si/No): "); 
+		while (agregarParametros.equalsIgnoreCase("si")) {
 			parametro = new Parametro();
 			parametro.setNombre(invocador.obtenerDatos("Ingrese el nombre del parametro: "));
 			//Necesito listar los validadores que tengo para los parametros
@@ -56,13 +85,28 @@ public class ComandosUsuarioFacade {
 			if (!regla.addParametro(parametro, validadores.get(indice))) {
 				invocador.mensaje("No pudo agregarse el nuevo parametro por favor intentelo de nuevo.");
 			}
+			agregarParametros = invocador.obtenerDatos("Desea especificar más parametros? (Si/No): ");	
 		}
-		try {
-			repositorioRegla.agregar(regla);
-		} catch (ValidacionExcepcion e) {
-			invocador.mensaje("No pudo guardarse la nueva regla, puede que ya exista. Por favor intentelo de nuevo.");
+	}
+
+	private void inicializar(InterfazUsuario invocador) {
+		if (repositorioRegla == null) {
+			repositorioRegla = new RepositorioReglas();
 		}
-		return "";
+		if (validadores == null) {
+			//reemplazar este new por la llamada a una factory o builder 
+			validadores = new ArrayList<ValidadorParametro>();
+			validadores = Converter.getValidadoresParametros(BuscadorClases.buscarClasesImplementanInterfaz(Constantes.NOMBRE_INTERFAZ_VALIDADOR, 
+														   Configuracion.DIRECTORIO_VALIDADOR_PARAMETROS_BASE, 
+														   Constantes.PAQUETE_INTERFAZ_VALIDADOR));
+		}
+		if (acciones == null) {
+			//reemplazar este new por la llamada a una factory o builder 
+			acciones = new ArrayList<String>();
+			acciones = BuscadorClases.buscarNombreClasesImplementanInterfaz(Constantes.NOMBRE_INTERFAZ_ACCION, 
+					   Configuracion.DIRECTORIO_ACCIONES_BASE, 
+					   Constantes.PAQUETE_INTERFAZ_VALIDADOR);
+		}
 	}
 	
 	/**
--- trunk/Tecnicas/src/ar/com/fi/uba/tecnicas/controlador/comun/Constantes.java
+++ trunk/Tecnicas/src/ar/com/fi/uba/tecnicas/controlador/comun/Constantes.java
@@ -2,18 +2,10 @@ package ar.com.fi.uba.tecnicas.controlador.comun;
 
 public class Constantes {
 
-	public static final String REGEX_ESLAVON_FINAL = "(^(E)(\\d)+$|(^\\()(E)(\\d)+(\\))$)";
-	public static final String REGEX_OR = "((E)(\\d)+OR(E)(\\d)+|(\\()(E)(\\d)+OR(E)(\\d)+(\\)))";
-	public static final String REGEX_AND = "((E)(\\d)+AND(E)(\\d)+|(\\()(E)(\\d)+AND(E)(\\d)+(\\)))";
-	public static final String REGEX_NOT = "(NOT(E)(\\d)+|NOT(\\()(E)(\\d)+(\\)))";
-	public static final String REGEX_XOR = "((E)(\\d)+XOR(E)(\\d)+|(\\()(E)(\\d)+XOR(E)(\\d)+(\\)))";
-	public static final String REGEX_PARENTESIS_EXTRA = "(\\()(E)(\\d)+(\\))";
-
-	//Regular expresion en que toda E este acompañada de un numero regexp -nocase -all -line -- {( |^|\()(E|e)(\d)*( |$|\))} string match v1 v2 v3 v4
-	public static final String REGEX_ENTRADAS_VALIDAS = "( |^|\\()(E|e)(\\d)+( |$|\\))";
-	public static final String REGEX_ENTRADAS_REALES= "(E|e)(\\d)+";
 	public static final String STRING_EMPTY = "";
-	
-	public static final Integer MAXIMO_NRO_ENTRADAS = 999;
-	
+	public static final String NOMBRE_INTERFAZ_VALIDADOR = "ValidadorParametro";
+	public static final String NOMBRE_INTERFAZ_ACCION = "Accion";
+	public static final String PAQUETE_INTERFAZ_VALIDADOR = "ar.com.fi.uba.tecnicas.controlador.validador";
+	public static final String PAQUETE_INTERFAZ_ACCION = "ar.com.fi.uba.tecnicas.modelo.entidades.accion";
+		
 }
