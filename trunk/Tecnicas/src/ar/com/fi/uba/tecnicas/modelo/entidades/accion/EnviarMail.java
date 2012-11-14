4e6ab491fd95f5e912891d1e3508758a84807057
ramiro.salom <ramiro.salom@616bce18-80df-bbd3-df9c-2f520cdca58d>
2012-11-14 00:25:45 +0000

    Agrego una accion vacia para poder probar que me liste las acciones

---
+++ trunk/Tecnicas/src/ar/com/fi/uba/tecnicas/modelo/entidades/accion/EnviarMail.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package ar.com.fi.uba.tecnicas.modelo.entidades.accion;
+
+import java.util.List;
+
+import ar.com.fi.uba.tecnicas.modelo.entidades.Mensaje;
+import ar.com.fi.uba.tecnicas.modelo.entidades.Parametro;
+
+/**
+ * @author ramiro
+ *
+ */
+public class EnviarMail implements Accion {
+
+	/**
+	 * @see ar.com.fi.uba.tecnicas.modelo.entidades.accion.Accion#ejecutar(ar.com.fi.uba.tecnicas.modelo.entidades.Mensaje, java.util.List)
+	 */
+	@Override
+	public void ejecutar(Mensaje mensaje, List<Parametro> parametros) {
+		
+	}
+
+}
