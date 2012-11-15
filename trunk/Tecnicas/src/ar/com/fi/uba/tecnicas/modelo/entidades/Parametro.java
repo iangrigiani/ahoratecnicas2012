6f9191b5b5da0d7aa88020b458f6d52eb1b9dd6d
ramiro.salom <ramiro.salom@616bce18-80df-bbd3-df9c-2f520cdca58d>
2012-11-15 16:58:10 +0000

    Agregado de equals y hashcode

--- trunk/Tecnicas/src/ar/com/fi/uba/tecnicas/modelo/entidades/Parametro.java
+++ trunk/Tecnicas/src/ar/com/fi/uba/tecnicas/modelo/entidades/Parametro.java
@@ -34,4 +34,35 @@ public class Parametro {
 		this.valor = valor;
 	}
 	
+	/**
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+	
+	/**
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Parametro other = (Parametro) obj;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
+	
 }
--- trunk/Tecnicas/src/ar/com/fi/uba/tecnicas/modelo/entidades/Regla.java
+++ trunk/Tecnicas/src/ar/com/fi/uba/tecnicas/modelo/entidades/Regla.java
@@ -126,4 +126,35 @@ public class Regla {
 		}
 		return ret;
 	}
+
+	/**
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+
+	/**
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Regla other = (Regla) obj;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
 }
