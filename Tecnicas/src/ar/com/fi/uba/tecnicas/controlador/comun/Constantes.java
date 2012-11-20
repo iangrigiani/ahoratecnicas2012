bd4fcaf3855998f982affa957dea39c8cd134a29
ignacio.angrigiani <ignacio.angrigiani@gmail.com>
2012-11-20 03:15:11 +0000

    Agregadas las acciones y las modificaciones que hizo ramiro en la biblioteca

--- Tecnicas/src/ar/com/fi/uba/tecnicas/controlador/comun/Constantes.java
+++ Tecnicas/src/ar/com/fi/uba/tecnicas/controlador/comun/Constantes.java
@@ -7,5 +7,7 @@ public class Constantes {
 	public static final String NOMBRE_INTERFAZ_ACCION = "Accion";
 	public static final String PAQUETE_INTERFAZ_VALIDADOR = "ar.com.fi.uba.tecnicas.controlador.validador";
 	public static final String PAQUETE_INTERFAZ_ACCION = "ar.com.fi.uba.tecnicas.modelo.entidades.accion";
-		
+	
+	//MENSAJES DE ERRORES
+	public static final String CANTIDAD_DE_PARAMETROS_INCORRECTO = "Cantidad de parametros incorrecto.";	
 }
